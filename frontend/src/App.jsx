b95eeec2251e7c2b94748896658c251fafbc89ea
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2024-10-01 13:52:39 -0600

    adding skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import { getUsers, getUser, createUser, updateUser, deleteUser } from './api';
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import { Feeds } from './pages/Feeds';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { Layout } from './components/layout';
 
 function App() {
   const [users, setUsers] = useState();
@@ -17,9 +22,17 @@ function App() {
   }, [])
 
   return (
-    <>
-     {JSON.stringify(users)}
-    </>
+    <Router>
+     {/* {JSON.stringify(users)} */}
+     <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route element={<Layout/>}>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/feeds" element={<Feeds/>}/>
+      </Route>
+     </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/pageData.js
@@ -0,0 +1,14 @@
+export const pageData = [
+    {
+        name: "Home",
+        path: "/home",   
+    },
+    {
+        name: "Profile",
+        path: "/profile",   
+    },
+    {
+        name: "Feeds",
+        path: "/feeds",   
+    },
+]
