5a37d6c05ab8338cd36447ddfd372d4ed128ec6a
Carlos Sujanto <carlos.sujanto@ucalgary.ca>
2024-10-01 14:00:45 -0600

    refactor for layout

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -4,10 +4,9 @@ import { pageData } from "./pageData"
 export function Navbar(){
     return(
         <div className="navbar">
-            aaaa
             {pageData.map((page) => {
                 return(
-                    <Link to={page.path} className="navItem">
+                    <Link to={page.path} key={page.path} className="navItem">
                         <button>
                             {page.name}
                         </button>
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import { getUsers, getUser, createUser, updateUser, deleteUser } from './api';
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
-import { Feeds } from './pages/Feeds';
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Layout } from './components/layout';
+import { Feeds } from '../pages/Feeds';
+import { Home } from '../pages/Home';
+import { Profile } from '../pages/Profile';
+import { Layout } from '../components/layout';
 
 function App() {
   const [users, setUsers] = useState();
@@ -25,9 +25,8 @@ function App() {
     <Router>
      {/* {JSON.stringify(users)} */}
      <Routes>
-      <Route path="/" element={<Landing/>}/>
+      <Route path="/home" element={<Home/>}/>
       <Route element={<Layout/>}>
-        <Route path="/home" element={<Home/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/feeds" element={<Feeds/>}/>
       </Route>
