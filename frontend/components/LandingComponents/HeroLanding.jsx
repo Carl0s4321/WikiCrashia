0c8656fac7de0608bfd47f5eddfed72c39cf3836
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-10-02 14:55:05 -0600

    Update HeroLanding.jsx

--- frontend/components/LandingComponents/HeroLanding.jsx
+++ frontend/components/LandingComponents/HeroLanding.jsx
@@ -7,10 +7,10 @@ export function HeroLanding() {
         <div className='flex flex-col md:flex-row items-center justify-between md:justify-center'>
         <div className='w-full md:w-6/12'>
               <div className='flex flex-col space-y-4 px-12 py-10'>
-                <h1 className='font-proximaBold text-7xl md:text-start sm:text-center'>Wiki</h1>
-                <h1 className='font-proximaBold text-7xl md:text-start sm:text-center'>Crashia</h1>
+                <h1 className='font-proximaBold text-7xl sm:text-center md:text-start'>Wiki</h1>
+                <h1 className='font-proximaBold text-7xl sm:text-center md:text-start'>Crashia</h1>
                 <p> We believe social media provides a dependable resource for real-time data, as users frequently post about accidents they witness or are involved in, offering an opportunity to respond to incidents faster.</p>
-                <button className='button-hero mx-auto' >
+                <button className='button-hero sm:mx-auto lg:mx-0' >
                   View map
                 </button>
               </div>
