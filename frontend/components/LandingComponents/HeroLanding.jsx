5aa6dbe5a16c151df993fa43cc789e7ad5552eb4
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-10-02 14:53:40 -0600

    finished hero landing for now

--- frontend/components/LandingComponents/HeroLanding.jsx
+++ frontend/components/LandingComponents/HeroLanding.jsx
@@ -4,23 +4,24 @@ export function HeroLanding() {
   return (
     <>
       <div className='bg-white text-black font-proxima'>
-        <div className='container mx-auto px-6 sm:px-4 md:px-6 lg:px-24'>
-          <div className='flex flex-col md:flex-row items-center justify-between'>
-            <div className='w-full md:w-6/12'>
-              <div className='pr-12'>
-              <h1 className='font-proximaBold text-7xl'>Wiki</h1>
-              <h1 className='font-proximaBold text-7xl'>Crashia</h1>
-              <p> We believe social media provides a dependable resource for real-time data, as users frequently post about accidents they witness or are involved in, offering an opportunity to respond to incidents faster.</p>
-              <button>
-                View map
-              </button>
+        <div className='flex flex-col md:flex-row items-center justify-between md:justify-center'>
+        <div className='w-full md:w-6/12'>
+              <div className='flex flex-col space-y-4 px-12 py-10'>
+                <h1 className='font-proximaBold text-7xl md:text-start sm:text-center'>Wiki</h1>
+                <h1 className='font-proximaBold text-7xl md:text-start sm:text-center'>Crashia</h1>
+                <p> We believe social media provides a dependable resource for real-time data, as users frequently post about accidents they witness or are involved in, offering an opportunity to respond to incidents faster.</p>
+                <button className='button-hero mx-auto' >
+                  View map
+                </button>
               </div>
             </div>
             <div className='relative second-div-hero w-full md:w-6/12 bg-black flex items-center justify-center'>
-              <img src={globe} alt="Globe" className="max-w-full max-h-full object-contain" />
+              <div>
+                <img src={globe} alt="Globe" className="max-w-full max-h-full object-contain" />
+              </div>
             </div>
-          </div>
         </div>
+
       </div>
     </>
   )
