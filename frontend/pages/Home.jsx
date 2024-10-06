853c2f975f2f4e33ddf0a3530bba106953a06bbe
chowdhuryrahat2 <72319198+chowdhuryrahat2@users.noreply.github.com>
2024-10-06 12:20:43 -0600

    Implemented map

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react"
+
+
 import {
     APIProvider, 
     Map, 
     AdvancedMarker, 
     Pin, 
     InfoWindow,
+    // MapCameraChangedEvent
 
 } from "@vis.gl/react-google-maps";
 export function Home() {
-    const position = {lat : 53.54, lng: 10};
+    const position = {lat : 51.049999, lng: -114.066666};
     const [open, setOpen] = useState(false);
+
+    const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+    const mapID = import.meta.env.VITE_MAP_ID;
+
        
     return(
-        <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-           <div className="font-proxima" style={{height: "100vh", width:"100%"}}>
+        <APIProvider apiKey={googleMapsApiKey} onLoad={() => console.log('Maps API has loaded.')}>           <div className="font-proxima" style={{height: "100vh", width:"100%"}}>
                 Home test font
                 <Map 
-                    zoom= {9} 
-                    center ={position} 
-                    mapId={process.env.REACT_APP_MAP_ID}>
+                    defaultZoom= {12} 
+                    defaultCenter ={position} 
+                    mapId={mapID}
+                    options= {{
+                        draggable: true,
+                        scrollwheel: true,
+                        zoomControl: true, 
+                        disableDoubleClickZoom: false
+                    }}
+                    >
                     <AdvancedMarker position = {position} onClick={() => setOpen(true)}>
                         <Pin 
                             background= {"red"}
@@ -30,6 +43,7 @@ export function Home() {
 
             </div>
         </APIProvider>
+        
     );
 }
 
